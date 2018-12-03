fe105d7fcdcb659cbb4a52dc183d3ff3644bc976
tazirahmb <tazirahmb@outlook.com>
2018-12-03 12:52:45 +0700

    Add header and button

--- App.js
+++ App.js
@@ -1,49 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {
+  StyleSheet,
+  Image,
+  View,
+  TouchableWithoutFeedback} from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Footer,
+  Text,
+  Button,
+  Left,
+  Body,
+  Right,
+  Icon
+} from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const colorData = {
+  white: '#FFFFFF',
+  splashHeader: '#283E4B',
+  splashBackground: '#344A57',
+  statusBarColor: '#016098',
+  lightBlue: '#1A8FC5'
+}
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      <Container style={styles.container}>
+        <Header noShadow style={styles.Header} androidStatusBarColor={colorData.statusBarColor}>
+          <Image
+            style={styles.logo}
+            source={require('./images/logo.png')}
+          />
+        </Header>
+        <Content contentContainerStyle={styles.content}>
+            <View styles={styles.splashImage}>
+              <Text style={{color: colorData.white}}>test</Text>
+            </View>
+            <View styles={styles.tombolTombol}>
+              <Button large iconLeft block light>
+                <Icon name='logo-google' />
+                <Text style={{color: colorData.lightBlue}}>JOIN WITH GOOGLE</Text>
+              </Button>
+              <Button large block bordered light>
+                <Text>JOIN NOW</Text>
+              </Button>
+              <TouchableWithoutFeedback>
+                <Text style={{color: colorData.white}}>Already have an account? 
+                  <Text style={{color: colorData.lightBlue}}> Sign In</Text>
+                </Text>
+              </TouchableWithoutFeedback>
+            </View>
+        </Content>
+        {/* <Footer /> */}
+      </Container>
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: colorData.splashBackground
+  },
+  Header: {
+    backgroundColor: colorData.splashHeader,
+    height: 45
+  },
+  logo: {
+      alignSelf: 'center',
+      scaleX: 0.75,
+      scaleY: 0.75
+  },
+  content: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
+  splashImage: {
+    flex: 1,
+    backgroundColor: 'yellow'
+  },
+  tombolTombol: {
+    flex: 1,
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
